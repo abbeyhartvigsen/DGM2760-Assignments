ad36bbf77eb80f02a70d71def7088d7232a51ae9
Abbey <abigail.hartvigsen>
2019-09-28 13:16:31 -0600

    css/figured out how to not have 0 correct number

--- Unit03/js/main.js
+++ Unit03/js/main.js
@@ -6,7 +6,7 @@
 
 //pick a random number between 0 and 16
 
-let correctNumber = Math.floor(Math.random() * 15)
+let correctNumber = Math.floor(Math.random() * 15 + 1)
 
 console.log(`The correctNumber is ${correctNumber}`)
 
@@ -28,12 +28,45 @@ function evalGuess() {
 
     if (gamerGuess == correctNumber) {
         console.log(`gamerGuess is equal to correctNumber`)
+        feedback.innerText = 'You win!'
+        giveAward()
     } else if (gamerGuess > correctNumber && gamerGuess < 16) {
         feedback.innerText = 'too high, try again'
     } else if (gamerGuess < correctNumber && gamerGuess > 0) {
         feedback.innerText = 'too low, try again'
     } else {
         feedback.innerText = 'Please choose a number between 1 and 15 and try again'
+        totalGuesses -= 1
     }
-        console.log(`Not equal`)
+        document.querySelector('#attempts').innerText = totalGuesses
+}
+
+function giveAward() {
+    console.log('Congratulations!')
+
+    switch(totalGuesses) {
+        case 1:
+        case 2:
+        case 3: 
+            console.log('Blue ribbon for you!')
+            break;
+        case 4: 
+            console.log('Case 4 just happened')
+            break;
+        case 5:
+        case 6: 
+        console.log('Red ribbon for you!')
+        break;
+        default: 
+        console.log('Yellow ribbon for you!')
+    }
+
+    const awardImage = document.createElement('img') //Creates an 'img'
+    awardImage.setAttribute('src', 'images/blue-ribbon-resizedr.jpg')
+
+
+    const ribbon = document.querySelector('#ribbon')
+
+    ribbon.appendChild(awardImage)
+
 }
