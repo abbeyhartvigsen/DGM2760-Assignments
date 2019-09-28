1c737e5f60f82e89f269db030ce91baa1f2c6c18
Abbey <abigail.hartvigsen>
2019-09-27 20:07:24 -0600

    got image to work and changed header text color

--- Unit03/js/main.js
+++ Unit03/js/main.js
@@ -1,13 +1,6 @@
 //document.getElementById('company').innerHTML = "Joe's Bed and Breakfast";
 
-document.querySelector('#company').innerText = "Joe's Bed and Breakfast";
+document.querySelector('#game').innerText = "Guessing Game";
 
-document.querySelector('header > h2').innerText = "Best Bed and Breakfast in Utah County";
+document.querySelector('header > h2').innerText = "Loops";
 
-let userName = prompt("What is your name?")
-
-// let message = "Hello " + userName + ", welcome to the best bed and breakfast in Utah County!"
-
-let message = `Hello ${userName}, welcome to the best bed and breakfast in Utah County!`
-
-document.querySelector('#greeting').innerText = message
