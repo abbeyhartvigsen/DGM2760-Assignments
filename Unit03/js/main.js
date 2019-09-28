d7aeb702bace434755f23a998555677321df70cd
Abbey <abigail.hartvigsen>
2019-09-27 20:48:26 -0600

    evalGuess function

--- Unit03/js/main.js
+++ Unit03/js/main.js
@@ -1,6 +1,24 @@
 //document.getElementById('company').innerHTML = "Joe's Bed and Breakfast";
 
-document.querySelector('#game').innerText = "Guessing Game";
+// document.querySelector('#game').innerText = "Guessing Game";
 
-document.querySelector('header > h2').innerText = "Loops";
+// document.querySelector('header > h2').innerText = "Loops";
 
+//pick a random number between 0 and 16
+
+const correctNumber = Math.floor(Math.random() * 15)
+
+console.log(`The correctNumber is ${correctNumber}`)
+
+let guessed = false
+
+let totalGuesses = 0
+
+let gamerGuess = 0
+
+function evalGuess {
+    totalGuesses += 1 // totalGuesses = totalGuesses + 1
+    gamerGuess = document.querySelector('#guess').nodeValue
+
+    const feedback = document.querySelector('#feedback')
+}
