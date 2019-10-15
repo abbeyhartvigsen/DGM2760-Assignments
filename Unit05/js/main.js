a2591cfa8dc8785f72c48b72c5f9178a5c835fae
Abbey <abigail.hartvigsen>
2019-10-15 17:03:01 -0600

    const listTrees

--- Unit05/js/main.js
+++ Unit05/js/main.js
@@ -3,3 +3,13 @@ const trees = ['oak', 'Pine', 'aspen', 'Bald Cypress']
 const errorElement = document.querySelector('#error')
 const displayResults = document.querySelector('#displayResults')
 
+//Display the list of trees inside the displayResults div
+const listTrees = () => {
+    let treeList = ''
+    trees.forEach(tree => {
+        treeList += `${tree} <br>`
+    })
+    displayResults.innerHTML = treeList
+}
+
+listTrees()
