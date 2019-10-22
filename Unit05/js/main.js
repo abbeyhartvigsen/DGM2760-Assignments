e5df748de77f4a18f435732cb7a70963c8633702
Abbey <abigail.hartvigsen>
2019-10-22 17:27:43 -0600

    finished buttons and error messages

--- Unit05/js/main.js
+++ Unit05/js/main.js
@@ -17,6 +17,7 @@ listTrees()
 document.querySelector('#add_redwood').onclick = () => {
     trees.push('redwood')
     listTrees()
+    CheckIfZero()
 }
 
 //add pear to the start
@@ -24,4 +25,92 @@ document.querySelector('#add_redwood').onclick = () => {
 document.querySelector('#add_pear').onclick = () => {
     trees.unshift('Pear')
     listTrees()
+    CheckIfZero()
 }
+
+document.querySelector('#remove_tree1').onclick = () => {
+    if (trees.length > 0) {
+        trees.shift()
+        listTrees()
+    } else {
+        // errorElement.textContent = 'I cannot remove the first tree cause there are no trees'
+        CheckIfZero('I cannot remove the first tree cause there are no trees', 0)
+    }
+}
+
+document.querySelector('#remove_tree2').onclick = () => {
+    if (trees.length > 1) {
+        trees.splice(1, 1)
+        listTrees()
+        console.log(trees.length)
+
+    }
+    else {
+        // errorElement.textContent = 'I cannot remove the second tree cause there are no trees'
+        CheckIfZero('I cannot remove the second tree because there is less than two trees', 2)
+    }
+}
+
+
+document.querySelector('#remove_treeLast').onclick = () => {
+    if (trees.length > 0) {
+        trees.pop()
+        listTrees()
+    } else {
+        // errorElement.textContent = 'I cannot remove the last tree cause there are no trees'
+        CheckIfZero('I cannot remove the last tree cause there are no trees', 0)
+    }
+}
+
+document.querySelector('#sortTrees').onclick = () => {
+    if (trees.length > 0) {
+        trees.sort()
+        listTrees()
+    } else
+        CheckIfZero('I cannot sort the trees because there are no trees', 0)
+}
+
+document.querySelector('#lowerTrees').onclick = () => {
+    if (trees.length > 0) {
+        console.log("click")
+        var sorted = [];
+        for (var i = 0; i < trees.length; i++) {
+            sorted.push(trees[i].toLowerCase());
+            trees.splice(i, 1, sorted[i])
+        }
+
+        sorted.sort();
+        listTrees()
+        // console.log(sorted)
+    } else
+        CheckIfZero('I cannot make all the trees lowercase because there are no trees', 0)
+}
+
+document.querySelector('#showName3').onclick = () => {
+    if (trees.length > 1) {
+        trees[2]
+        listTrees()
+    } else 
+    CheckIfZero('I cannot give the third tree because there is less than three trees',1)
+}
+
+document.querySelector('#showName4').onclick = () => {
+    if (trees.length > 2) {
+        trees[3]
+        listTrees()
+    } else 
+    CheckIfZero('I cannot give the fourth tree because there is less than four trees',2)
+}
+
+
+
+    function CheckIfZero(errorMessage, numberToCheck) {
+
+        if (trees.length <= numberToCheck) {
+            errorElement.textContent = errorMessage
+
+        } else {
+            errorElement.textContent = ""
+        }
+
+    }
