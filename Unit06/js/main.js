43a6d31b02deeb80da3791017553afe149352490
Abbey <abigail.hartvigsen>
2019-10-29 15:59:31 -0600

    started project

---
+++ Unit06/js/main.js
@@ -0,0 +1,7 @@
+function tellStory() {
+    //noun Array = querySelector('#noun') //lowercase String split()
+    //repeat for adjectives and verbs
+
+    const myStory = `There once lived some ${nounArray[0]}s. They had ${adjectiveArray[0]} ${nounArray[1]}`
+    //get a reference to the querySelector('#story') = myStory
+}
