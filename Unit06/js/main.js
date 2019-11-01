e9e2c502706fe03728cc3626261d3f0f2396677e
Abbey <abigail.hartvigsen>
2019-11-01 15:51:47 -0600

    fixed the story

--- Unit06/js/main.js
+++ Unit06/js/main.js
@@ -21,7 +21,7 @@ function tellStory() {
 
    // var verb = document.querySelector('#verb').split(/\s+/);
 
-    const myStory = `There once lived some ${nounArray[0]}s. They had ${adjectiveArray[0]} ${nounArray[1]}s. One day some ${adjectiveArray[1]} ${nounArray[2]}s ${verbArray[0]} and then ${verbArray[1]}. Then the ${nounArray[0]} said to them, "Hello, you are a very ${adjectiveArray[2]} ${nounArray[2]}. Yesterday I saw a ${nounArray[3]} and a ${nounArray[4]}." The ${nounArray[2]} said, "That's interesting! I also saw a ${nounArray[5]} and a ${nounArray[6]}." The ${nounArray[0]} said, "That's interesting" and then they took the ${nounArray[2]} to the ${nounArray[2]}'s favorite place.`;
+    const myStory = `There once lived some ${nounArray[0]}s. They had ${adjectiveArray[0]} ${nounArray[1]}s. One day some ${adjectiveArray[1]} ${nounArray[2]}s ${verbArray[0]} and then ${verbArray[1]}. Then a ${nounArray[0]} said to them, "Hello, you are very ${adjectiveArray[2]} ${nounArray[2]}s. Yesterday I saw a ${nounArray[3]} and a ${nounArray[4]}." A ${nounArray[2]} said, "That's interesting! I also saw a ${nounArray[5]} and a ${nounArray[6]}." The ${nounArray[0]}s said, "That's interesting" and then they took the ${nounArray[2]}s to the ${nounArray[2]}'s favorite place.`;
     console.log(myStory)
     //get a reference to the querySelector('#story') = myStory
     let storyContainer = document.querySelector('#story')
